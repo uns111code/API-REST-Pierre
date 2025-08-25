6c1b9dc8fb01b40b3000a2a8c0f0589efda92e80
uns111code <uns.code111@gmail.com>
2025-08-25 09:59:22 +0200

    chore: remove unused Json constraint import in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Constraints\Json;
 
 class UserController extends AbstractController
 {
